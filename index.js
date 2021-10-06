c51d475d8c4ab847b7cf56283947e913488b0bc1
Victor Rodrigues <victorassisrd@gmail.com>
2021-10-06 09:44:06 -0300

    Solucao CORS

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const mysql  = require('mysql');
 // Permitir que o server trabalhe com JSON
 app.use(express.json());
 
+server.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", "*");
+    server.use(cors());
+    next();
+});
+
 // Carregar informações de conexão
 require('dotenv-safe').config();
 
